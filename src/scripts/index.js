4855b7509918eaac6d23fad36bc9251d1f43574d
Rose A Hill <rosearnagahill@gmail.com>
2020-07-15 01:41:16 -0400

    Added base style & contact/copy buttons

---
+++ src/scripts/index.js
@@ -0,0 +1,45 @@
+
+/* Display mode handler */
+
+function toggleMode() {
+    const bodyElm = document.body;
+    bodyElm.classList.toggle("lightmode");
+}
+
+/* Copy handler */
+
+function textCopy(ele, id) {
+
+    const icoEle = ele.querySelector('svg');
+    //DEBUG
+    console.log(icoEle);
+    //
+    
+    icoEle.classList.add('fade');
+
+    setTimeout(() => {
+        icoEle.classList.remove('fade');
+    }, 1000);
+    
+    const copyText = document.getElementById(id);
+    //DEBUG
+    console.log(copyText);
+    //
+    
+    copyText.style.display = "inline";
+    copyText.select();
+
+    try {
+        var successful = document.execCommand('copy', false, 'Bleh');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Unable to copy text...');
+    }
+
+    copyText.style.display = "none";
+}
+
+//DEBUG
+console.log('javaScript loaded');
+//
